7df757b023eb296492c4e489bd221236f904288b
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-08-24 13:42:06 +0530

    triangle validation logic building

--- isTriangle.js
+++ isTriangle.js
@@ -1,11 +1,25 @@
-console.log("its working!");
 
 const inputs = document.querySelectorAll(".angle-input");
 const isTriangleBtn = document.querySelector("#is-triangle-btn");
 const output = document.querySelector("#output");
 
+calculateSumOfAngles = (angle1, angle2, angle3) => {
+    const sumOfAngles = angle1 + angle2 + angle3;
+    return sumOfAngles;
+}
+
 isTriangle = () => {
-    console.log("triangle formed");
+
+    const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
+
+    // console.log(sumOfAngles);
+
+    if (sumOfAngles === 180) {
+        output.innerHTML = ("Angles form a Triangle !")
+    } else {
+        output.innerHTML = ("Angles not form a triangle...")
+    }
+
 }
 
 isTriangleBtn.addEventListener("click", isTriangle)
