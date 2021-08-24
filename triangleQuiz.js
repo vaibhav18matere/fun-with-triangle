3d173b8c81937f5d08f0d4a671bb8f864aac03cc
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-08-24 17:00:54 +0530

    quiz logic built

---
+++ triangleQuiz.js
@@ -0,0 +1,22 @@
+console.log("Let's go!");
+
+const quizForm = document.querySelector(".quiz-form");
+const submitAnswerBtn = document.querySelector("#submit-answer-btn");
+const output = document.querySelector("#output");
+
+const correctAnswer = ["90°", "Right angled"];
+
+function calculateScore() {
+    let score = 0;
+    let index = 0;
+    const formResults = new FormData(quizForm);
+    for (let optionSelected of formResults.values()) {
+        if (optionSelected === correctAnswer[index]) {
+            score = score + 1;
+        }
+        index = index + 1;
+    }
+    output.innerHTML = "Your score is : " + score;
+}
+
+submitAnswerBtn.addEventListener("click", calculateScore);
