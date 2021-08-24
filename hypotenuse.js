f9b208f90339ed580e7e71348651a22353e15653
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-08-24 17:51:52 +0530

    took input from html and calculate  sum of squares

--- hypotenuse.js
+++ hypotenuse.js
@@ -1,1 +1,18 @@
 console.log("lets start");
+
+const inputSides = document.querySelectorAll(".side-input");
+const hypotenuseBtn = document.querySelector("#hypotenuse-button");
+const outputValue = document.querySelector("#output");
+
+calculateSumOfSquares = (side1, side2) => {
+    const sumOfSquares = side1 ** 2 + side2 ** 2;
+    return sumOfSquares;
+}
+
+calculateHypotenuse = () => {
+
+    const sumOfSquares = calculateSumOfSquares(2, 8);
+    console.log(sumOfSquares);
+}
+
+hypotenuseBtn.addEventListener("click", calculateHypotenuse)
